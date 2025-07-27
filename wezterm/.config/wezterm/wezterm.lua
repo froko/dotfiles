99abf0a9b29053ee80d063050dd093de3f10b8fb
Patrick Ineichen <github@frokonet.ch>
2025-07-27 13:24:19 +0200

    feat(wezterm): add wezterm configuration

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,26 @@
+local wezterm = require("wezterm")
+
+local config = {
+	color_scheme = "Catppuccin Mocha",
+	font = wezterm.font("JetBrains Mono"),
+	enable_tab_bar = false,
+	native_macos_fullscreen_mode = true,
+	window_decorations = "RESIZE",
+	window_background_opacity = 0.9,
+	keys = {
+		{
+			key = "n",
+			mods = "SHIFT|CTRL",
+			action = wezterm.action.ToggleFullScreen,
+		},
+	},
+	mouse_bindings = {
+		{
+			event = { Up = { streak = 1, button = "Left" } },
+			mods = "CTRL",
+			action = wezterm.action.OpenLinkAtMouseCursor,
+		},
+	},
+}
+
+return config
