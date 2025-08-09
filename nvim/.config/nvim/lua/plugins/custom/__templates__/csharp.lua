1b7691a0596f34e7ca713fe013d4da6c634b0790
Patrick Ineichen <github@frokonet.ch>
2025-08-09 17:17:38 +0200

    feat(nvim): add csharp support

---
+++ nvim/.config/nvim/lua/plugins/custom/__templates__/csharp.lua
@@ -0,0 +1,27 @@
+vim.lsp.enable('csharp-ls')
+
+return {
+  {
+    'WhoIsSethDaniel/mason-tool-installer.nvim',
+    opts = function(_, opts)
+      local tools = { 'csharp-language-server', 'csharpier' }
+      opts.ensure_installed = vim.list_extend(opts.ensure_installed or {}, tools)
+    end,
+  },
+  {
+    'nvim-treesitter/nvim-treesitter',
+    opts = function(_, opts)
+      local parsers = { 'c_sharp' }
+      opts.ensure_installed = vim.list_extend(opts.ensure_installed or {}, parsers)
+      opts.markdown = { enable = true }
+      opts.markdown_inline = { enable = true }
+    end,
+  },
+  {
+    'stevearc/conform.nvim',
+    opts = function(_, opts)
+      local formatters = { c_sharp = { 'csharpier' } }
+      opts.formatters_by_ft = vim.tbl_deep_extend('force', opts.formatters_by_ft or {}, formatters)
+    end,
+  },
+}
