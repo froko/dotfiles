97e7b45bc10227762c79a82b39bb5f3b99389619
Patrick Ineichen <github@frokonet.ch>
2026-01-23 13:31:31 +0100

    feat(wezterm): set default font size to 14

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -3,6 +3,7 @@ local wezterm = require("wezterm")
 local config = {
 	color_scheme = "Catppuccin Mocha",
 	font = wezterm.font("JetBrains Mono"),
+	font_size = 14.0,
 	enable_tab_bar = false,
 	native_macos_fullscreen_mode = true,
 	window_decorations = "RESIZE",
