0f7a5e60dbfd3584ae78fd6cf1b8e4dfe2b4e201
Patrick Ineichen <github@frokonet.ch>
2026-02-22 21:28:26 +0100

    fix(nvim): lsp integration

--- nvim/.config/nvim/lua/config/lsp.lua
+++ nvim/.config/nvim/lua/config/lsp.lua
@@ -1,16 +1,50 @@
 local nnoremap = require('utils').nnoremap
+local vnoremap = require('utils').vnoremap
 
-local coding_plugins_loaded, blink = pcall(require, 'blink.cmp')
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  callback = function(args)
+    local bufnr = args.buf
 
-if (coding_plugins_loaded == true) then
-  vim.lsp.enable('lua_ls')
-  vim.lsp.config('*', { capabilities = blink.get_lsp_capabilities() })
+    local function is_ts_like(ft)
+      return ft == 'typescript' or ft == 'typescriptreact' or ft == 'javascript' or ft == 'javascriptreact'
+    end
 
-  vim.api.nvim_create_autocmd('LspAttach', {
-    callback = function(ev)
-      nnoremap('gd', vim.lsp.buf.definition, { buffer = ev.buf, desc = '[G]oto [D]efinition' })
-    end,
-  })
+    local function gra()
+      local ft = vim.bo[bufnr].filetype
 
-  vim.diagnostic.config({ virtual_lines = true })
-end
+      if is_ts_like(ft) then
+        vim.lsp.buf.code_action({
+          context = {
+            only = { 'quickfix', 'refactor', 'source' },
+            diagnostics = vim.diagnostic.get(bufnr),
+          },
+        })
+      else
+        vim.lsp.buf.code_action()
+      end
+    end
+
+    nnoremap('gd', vim.lsp.buf.definition, { buffer = bufnr, desc = 'LSP: Go to definition' })
+    nnoremap('gra', gra, { buffer = bufnr, desc = 'LSP: Code action' })
+    vnoremap('gra', gra, { buffer = bufnr, desc = 'LSP: Code action' })
+    nnoremap('gri', vim.lsp.buf.implementation, { buffer = bufnr, desc = 'LSP: Implementation' })
+    nnoremap('grn', vim.lsp.buf.rename, { buffer = bufnr, desc = 'LSP: Rename' })
+    nnoremap('grr', vim.lsp.buf.references, { buffer = bufnr, desc = 'LSP: References' })
+    nnoremap('grt', vim.lsp.buf.type_definition, { buffer = bufnr, desc = 'LSP: Go to type definition' })
+    nnoremap('K', vim.lsp.buf.hover, { buffer = bufnr, desc = 'LSP: Hover' })
+  end,
+})
+
+local severity = vim.diagnostic.severity
+vim.diagnostic.config({
+  virtual_lines = true,
+  signs = {
+    text = {
+      [severity.ERROR] = ' ',
+      [severity.WARN] = ' ',
+      [severity.HINT] = '󰠠 ',
+      [severity.INFO] = ' ',
+    },
+  },
+})
---
+++ nvim/.config/nvim/lua/plugins/coding/lua.lua
@@ -0,0 +1,26 @@
+vim.lsp.enable('lua_ls')
+
+return {
+  {
+    'WhoIsSethDaniel/mason-tool-installer.nvim',
+    opts = function(_, opts)
+      local tools = { 'lua-language-server', 'stylua' }
+      opts.ensure_installed = vim.list_extend(opts.ensure_installed or {}, tools)
+    end,
+  },
+  {
+    'nvim-treesitter/nvim-treesitter',
+    opts = function(_, opts)
+      local parsers = { 'lua', 'vim' }
+      opts.ensure_installed = vim.list_extend(opts.ensure_installed or {}, parsers)
+      return opts
+    end,
+  },
+  {
+    'stevearc/conform.nvim',
+    opts = function(_, opts)
+      local formatters = { lua = { 'stylua' } }
+      opts.formatters_by_ft = vim.tbl_deep_extend('force', opts.formatters_by_ft or {}, formatters)
+    end,
+  },
+}
