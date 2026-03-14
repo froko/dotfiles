74c85f6d882cffaf98336706ea28cc817dbca349
Patrick Ineichen <patrick.ineichen@contractors.roche.com>
2026-03-14 22:57:45 +0100

    feat(wezterm): update configuration

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,27 +1,28 @@
 local wezterm = require("wezterm")
 
 local config = {
-	color_scheme = "Catppuccin Mocha",
-	font = wezterm.font("JetBrains Mono"),
-	font_size = 14.0,
-	enable_tab_bar = false,
-	native_macos_fullscreen_mode = true,
-	window_decorations = "RESIZE",
-	window_background_opacity = 0.9,
-	keys = {
-		{
-			key = "n",
-			mods = "SHIFT|CTRL",
-			action = wezterm.action.ToggleFullScreen,
-		},
-	},
-	mouse_bindings = {
-		{
-			event = { Up = { streak = 1, button = "Left" } },
-			mods = "CTRL",
-			action = wezterm.action.OpenLinkAtMouseCursor,
-		},
-	},
+  window_close_confirmation = "NeverPrompt",
+  color_scheme = "Catppuccin Mocha",
+  font = wezterm.font("JetBrains Mono"),
+  font_size = 16.0,
+  enable_tab_bar = false,
+  native_macos_fullscreen_mode = true,
+  window_decorations = "RESIZE",
+  window_background_opacity = 0.9,
+  keys = {
+    {
+      key = "n",
+      mods = "SHIFT|CTRL",
+      action = wezterm.action.ToggleFullScreen,
+    },
+  },
+  mouse_bindings = {
+    {
+      event = { Up = { streak = 1, button = "Left" } },
+      mods = "CTRL",
+      action = wezterm.action.OpenLinkAtMouseCursor,
+    },
+  },
 }
 
 return config
