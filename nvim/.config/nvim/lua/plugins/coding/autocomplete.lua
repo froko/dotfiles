fefc9a45832bb8b0f832c7255ccb7d246a224c4c
Patrick Ineichen <github@frokonet.ch>
2026-03-27 15:10:25 +0100

    feat(nvim): make default autocomplete keymaps explicit

--- nvim/.config/nvim/lua/plugins/coding/autocomplete.lua
+++ nvim/.config/nvim/lua/plugins/coding/autocomplete.lua
@@ -4,5 +4,13 @@ return {
   opts = {
     fuzzy = { implementation = 'lua' },
     sources = { default = { 'lsp', 'buffer', 'path' } },
+    keymap = {
+      preset = 'default',
+      ['<C-x><C-o>'] = { 'show', 'show_documentation', 'hide_documentation' },
+      ['<C-e>'] = { 'hide' },
+      ['<C-y>'] = { 'select_and_accept' },
+      ['<C-n>'] = { 'select_next', 'fallback' },
+      ['<C-p>'] = { 'select_prev', 'fallback' },
+    },
   },
 }
