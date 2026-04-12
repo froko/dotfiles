7b3018fb223e9f8af1a99f2260392953e9e58aa8
Patrick Ineichen <github@frokonet.ch>
2026-04-12 14:42:04 +0200

    feat: fall back to native autocompletion if blink.cmp is not active

--- nvim/.config/nvim/lua/coding/lsp.lua
+++ nvim/.config/nvim/lua/coding/lsp.lua
@@ -6,5 +6,14 @@ vim.api.nvim_create_autocmd('LspAttach', {
 
     nnoremap('gd', vim.lsp.buf.definition, { buffer = bufnr, desc = 'LSP: Go to definition' })
     nnoremap('grr', fzf.lsp_references, { buffer = bufnr, desc = 'LSP: References' })
+
+    local has_blink, _ = pcall(require, 'blink.cmp')
+    if not has_blink then
+      vim.bo[args.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+      local client = vim.lsp.get_client_by_id(args.data.client_id)
+      vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
+    end
   end,
 })
+
+vim.opt.completeopt = { 'menuone', 'noselect', 'noinsert' }
