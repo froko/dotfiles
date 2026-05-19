d2637dca7cf4af15c004b534cf5290b65cad9624
Patrick Ineichen <github@frokonet.ch>
2026-05-19 11:11:03 +0200

    fix(nvim): statusline seperators

--- nvim/.config/nvim/lua/essentials.lua
+++ nvim/.config/nvim/lua/essentials.lua
@@ -35,8 +35,8 @@ vim.cmd.colorscheme('catppuccin-macchiato')
 require('lualine').setup({
   options = {
     theme = 'auto',
-    component_separators = '',
-    section_separators = { left = '', right = '' },
+    component_separators = '',
+    section_separators = { left = '', right = '' },
   },
 })
 
